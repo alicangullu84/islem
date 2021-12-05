c908463f54ac90e35bb1356ac1e4bec1c25ae9cb
alicangullu84 <95480731+alicangullu84@users.noreply.github.com>
2021-12-05 13:07:02 +0300

    sürüm 0.1

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1638698667,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/soru-sheet0.png",
+		"images/a-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/spritefont.png",
+		"images/zaman-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
